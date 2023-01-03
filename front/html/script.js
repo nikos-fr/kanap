a91fbd21b5542aa9ecf7df7bf1a16caab9536e47
nikosfr <nicolousa.dev@gmail.com>
2023-01-03 20:58:06 +0100

    first commit

---
+++ front/html/script.js
@@ -0,0 +1,28 @@
+// 1 recuperer les article
+// 2 construire html
+// 3 injecter html dans le dom
+// 3.1 pointer sur element items
+// 3.2 INJECTER DANS LE DOM
+
+let a = fetch("http://localhost:3000/api/products")
+  .then(res => res.json())
+    .then(data => {
+        let display = ''
+        for(let article of data){
+            display +=`
+            <a href="./product.html?id=${article._id}">
+            <article>
+              <img src="${article.imageUrl}" alt="${article.altTxt}">
+              <h3 class="productName">${article.name}</h3>
+              <p class="productDescription">${article.description}</p>
+            </article>
+          </a>
+            `
+        }
+        document.querySelector("#items").innerHTML = display
+    })
+  .catch(err => {
+      console.log("dans le catch")
+      console.log(err)
+  })
+console.log(a)
