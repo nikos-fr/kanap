f33d6d72cfd6714e182d305e1dc7440c546f07dc
Hugo STK <hugobrionne@gmail.com>
2023-01-12 21:54:16 +0900

    Correction session

--- front/js/product.js
+++ front/js/product.js
@@ -23,8 +23,33 @@ let requestProduct = fetch("http://localhost:3000/api/products/" + productId)
       console.log(colorOption);
     }
   })
+
 // clic du bouton ajouter au panier 
 const addToCart = document.querySelector(".item__content__addButton");
-addToCart.addEventListener('click', () => {
-  console.log("click");
-});
+addToCart.addEventListener('click', saveOrder)
+
+
+// 1- Créer une fonction pour stocker la key et value pour chaques produit et de push en localstorage
+// 2- Executer la fonction quand la personne click sur le CTA
+// 3- Vérifier qu'une option est bien séléctionné et une quantitée avant le click / Sinon envoyer une alerte
+// 4- Créer une fonction globale avec queryselector pour prendre les valeurs et vérifier que toutes les conditions sont bien remplies avant du push en local storage
+
+const quantity = document.querySelector(#quantity).value
+const color = document.querySelector(#colors).value
+const price = document.querySelector(#price).value
+
+function saveOrder(color, quantity){
+  // const quantity = document.querySelector(#quantity).value
+  // const color = document.querySelector(#colors).value
+  // const price = document.querySelector(#price).value
+  const key = '${productId}_${color}'
+  const value = {
+      color: color,
+      quantity: quantity,
+      price: price,
+      imageUrl: XX,
+      altTxt: XX,
+      name: XX
+  }
+  localStorage.setItem(key, JSON.stringify(value))
+}
